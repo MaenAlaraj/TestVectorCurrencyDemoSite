34ca8cd3c12b6b0b252305259f0b3cd6438f1419
maen.alaraj <maen.alaraj@gck.co.jp>
2025-03-04 16:48:37 +0900

    Initial commit

--- src/components/FaceBTN.jsx
+++ src/components/FaceBTN.jsx
@@ -1,8 +1,12 @@
 // components/FaceBTN.jsx
-import React from "react";
 import { setMessage } from "../utils"; // Import setMessage from utils.js
+import React, { useState } from "react"; // Import useState
 
 const FaceBTN = ({ memberId, purchaseAmount, sbUserId, storeCode }) => {
+     // State to hold the base64 image data
+  const [faceImage, setFaceImage] = useState(null);
+
+
   const handleButtonClick = async () => {
     console.log("[Face Recognition]: Authentication initiated.");
 
@@ -33,7 +37,13 @@ const FaceBTN = ({ memberId, purchaseAmount, sbUserId, storeCode }) => {
           console.log("[FaceButton]:Captured Nr. of Faces:", numberOfFaces);
           const base64String = facesArray[0].image64;
           console.log("[FaceButton]:base64String value is:", base64String);
-          await window.CCWalletInterface.VectorCurrencyManagement(base64String,purchaseAmount,sbUserId, storeCode);
+
+
+            // Set the base64 image to state
+            setFaceImage(base64String);
+
+            await window.CCWalletInterface.VectorCurrencyManagement(memberId,purchaseAmount,sbUserId, storeCode, base64String);
+
         } else if (numberOfFaces > 1) {
           setMessage("複数の顔が検出されました。もう一度試してください。", "showDialog");
         }
@@ -54,12 +64,37 @@ const FaceBTN = ({ memberId, purchaseAmount, sbUserId, storeCode }) => {
   };
 
   return (
+    <div className="w-full text-center">
     <button
       onClick={handleButtonClick}
       className="w-full text-2xl font-bold bg-green-600 text-white py-4 rounded-xl hover:bg-green-700 transition-all shadow-md mt-4"
     >
       Face
     </button>
+
+    {/* Display the face image if available */}
+    {faceImage && (
+      <div className="mt-6">
+        <img
+          src={`data:image/jpeg;base64,${faceImage}`}
+          alt="Captured Face"
+          className="rounded-lg shadow-lg max-w-full max-h-[400px] object-cover mx-auto mt-4"
+        />
+      </div>
+    )}
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
   );
 };
 
